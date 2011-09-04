9e6fd546ba6bdbbcf9f06fbc5f18da9f9841dff7
Jeraimee <a.sleep@asleep.net>
2011-09-03 22:37:59 -0400

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wltt::Application.config.secret_token = 'fcd650676c75a30f3279bad295c2ead41b02f9003e24fd7944ee7aec198e23a0f2fdbbe7933ba21b00a94495bcf4948f12ba5c9dcd90c292daf3d1363c4f5659'
