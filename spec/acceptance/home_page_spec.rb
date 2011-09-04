b42cb6953203f449f672f5c9d723e9ba724aebbe
Jeraimee <a.sleep@asleep.net>
2011-09-03 23:33:40 -0400

    Added home page spec

---
+++ spec/acceptance/home_page_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+feature 'Home Page', %q{
+  As an unregistered user
+  I want to see the home/landing page
+} do
+  background do
+    # Nothing to do here
+  end
+  scenario "Home page" do
+    visit "/"
+    page.should have_link('Sign In')
+    page.should have_selector('title', :content => "WeLikeToTravel")
+    page.should have_content('What is WeLikeToTravel?')
+  end
+end
